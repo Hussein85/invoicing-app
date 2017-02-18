5faf179e817dfe5eca44d8e6b2f27e7a215030d9
Hussein <alhadad.hussein@gmail.com>
2017-02-18 22:42:38 +0100

    table columns are now sortable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,23 @@ export class AppComponent implements OnInit {
     invoices: Invoice[];
     isClosed: Boolean;
 
+    sortedInvoices: Invoice[];
+    sortBy = "";
+      
+    ascending = { 
+      type: true, 
+      accountName: true,
+      status: true,
+      currency: true,
+      balance: true  
+    };
+
+
     constructor(private invoiceService:InvoiceService ) { }
 
     ngOnInit() {
       this.invoices = this.invoiceService.getInvoices();
+      this.sortedInvoices = this.invoices.slice();
     }
 
     onSelect(invoice){
@@ -27,6 +40,23 @@ export class AppComponent implements OnInit {
 
     close(isClosed: Boolean) {
         this.isClosed = isClosed;
+    }
+
+    sort(sortBy) {
+      if(this.ascending[sortBy]){
+          this.sortedInvoices.sort(function(a, b) {
+            var x = a[sortBy]; var y = b[sortBy];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+          });
+      }else{
+        this.sortedInvoices.sort(function(a, b) {
+            var x = a[sortBy]; var y = b[sortBy];
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+          });
+      }
+
+      this.ascending[sortBy] = !this.ascending[sortBy]
+     
     }
 
 }
